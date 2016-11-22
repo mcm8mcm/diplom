daff918aaee5e80398d2c860e8b9ffcfc5353bc7
MCM <mcm@mcm-nb>
2016-11-22 22:12:46 +0200

    + engine classes

--- app/index.php
+++ app/index.php
@@ -1,17 +1,8 @@
 <?php 
-	include 'init.php';
-	include_once ENGINE_PATH.DS.'Router.php';
-	$router = new Router();
-?>
-<html>
-<head>
-<title>SAND BOX</title>
-</head>
-<body>
-<?php 
-$action = $router->getAction();
-$controller = $router->getController();
-$controller->$action();
-?>
-	</body>
-</html>
+require_once './config.php';
+
+require_once './init.php';
+
+$register = new Register();
+$loader = new Loader($register);
+$register->set('load', $loader);
--- app/init.php
+++ app/init.php
@@ -1,6 +1,4 @@
 <?php
-include_once 'config.php';
-
 function autoloader_lib($class_name){
 	$path = str_replace('\\', '/', strtolower(SYSTEM_PATH.'lib'.DS.$class_name)).'.php';
 	if(is_file($path)){
@@ -9,4 +7,12 @@ function autoloader_lib($class_name){
 }
 
 spl_autoload_register('autoloader_lib');
+
+require_once ENGINE_PATH.'register.php';
+require_once ENGINE_PATH.'controller.php';
+require_once ENGINE_PATH.'loader.php';
+require_once ENGINE_PATH.'model.php';
+require_once ENGINE_PATH.'session.php';
+require_once ENGINE_PATH.'response.php';
+require_once ENGINE_PATH.'request.php';
 
