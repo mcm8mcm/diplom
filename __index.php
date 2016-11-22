3877f79bd4c3c17dae6f6efd7bd138f50aad0ccb
MCM <mcm@mcm-nb>
2016-11-22 22:13:37 +0200

    + engine classes

---
+++ __index.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<title>SAND BOX</title>
+	</head>
+	<body>
+		<h1>MIST ME !!! </h1>
+                <?php var_export($_SERVER); ?>
+                <?php phpinfo(); ?>
+	</body>
+</html>
