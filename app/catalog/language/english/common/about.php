9511b5c88ffb1a863e420fe7c4a9717a642ac865
MCM <mcm@mcm-nb>
2017-01-01 15:40:24 +0200

    About 0.1.

--- app/catalog/language/english/common/about.php
+++ app/catalog/language/english/common/about.php
@@ -3,4 +3,7 @@ $_['title'] = 'About us';
 $_['about'] = 'The subject of our company is fixing of broken electronic devices ang gadgets'
         . 'Our company, also do mounting of some kinds of climate control equiptment.'
         . 'We are the official partners of some world-wide known companies e.g.:'
-        . 'Samsung, LG, Saturn etc.';
+        . 'Samsung, LG, Saturn etc.'
+        . 'Here downward are some of our prices.'
+        . 'You can ask a full set of prices to our managers.'
+        . 'The contacts of our managers you, also, can find on this site.';
