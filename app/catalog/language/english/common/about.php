318e8ba21961b7e384f7aa8fd1fa6b2c39239d45
MCM <mcm@mcm-nb>
2017-01-01 15:34:39 +0200

    About.

---
+++ app/catalog/language/english/common/about.php
@@ -0,0 +1,6 @@
+<?php
+$_['title'] = 'About us';
+$_['about'] = 'The subject of our company is fixing of broken electronic devices ang gadgets'
+        . 'Our company, also do mounting of some kinds of climate control equiptment.'
+        . 'We are the official partners of some world-wide known companies e.g.:'
+        . 'Samsung, LG, Saturn etc.';
