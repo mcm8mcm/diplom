b6186b4388c9197ba075378e6a3903e3310df815
MCM <mcm@mcm-nb>
2017-01-24 00:34:29 +0200

    Main tables added.

--- service.sql
+++ service.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 27, 2016 at 10:55 
+-- Generation Time: Jan 23, 2017 at 11:32 
 -- Server version: 10.1.19-MariaDB
 -- PHP Version: 5.6.24
 
@@ -19,6 +19,19 @@ SET time_zone = "+00:00";
 --
 -- Database: `service`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `device`
+--
+
+CREATE TABLE `device` (
+  `id` int(11) NOT NULL,
+  `serial` varchar(25) NOT NULL,
+  `devicetype` varchar(45) NOT NULL,
+  `brand` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -85,6 +98,69 @@ INSERT INTO `options` (`name`, `value`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `order`
+--
+
+CREATE TABLE `order` (
+  `idorder` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `start_date` date NOT NULL,
+  `close_date` date DEFAULT NULL,
+  `device_id` int(11) NOT NULL,
+  `device_condition` varchar(64) NOT NULL,
+  `complaint` varchar(64) NOT NULL,
+  `completeness` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_log`
+--
+
+CREATE TABLE `order_log` (
+  `id` int(11) NOT NULL,
+  `oreder_id` int(11) NOT NULL,
+  `post_stamp` varchar(45) NOT NULL DEFAULT 'NOW()',
+  `post_author` int(11) NOT NULL,
+  `post_reciver` int(11) DEFAULT NULL,
+  `post_title` varchar(64) DEFAULT 'Без заголовка',
+  `post_content` varchar(255) NOT NULL DEFAULT 'Нечего написать'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prices`
+--
+
+CREATE TABLE `prices` (
+  `id` int(11) NOT NULL,
+  `price_name` varchar(64) NOT NULL DEFAULT 'some_price',
+  `price_desc` varchar(64) NOT NULL DEFAULT 'some_price',
+  `lang_id` varchar(16) NOT NULL DEFAULT 'english',
+  `file_name` varchar(45) NOT NULL DEFAULT 'default',
+  `actual` int(1) NOT NULL DEFAULT '0',
+  `codepage` varchar(8) NOT NULL DEFAULT 'UTF-8'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `prices`
+--
+
+INSERT INTO `prices` (`id`, `price_name`, `price_desc`, `lang_id`, `file_name`, `actual`, `codepage`) VALUES
+(1, 'Monitors', 'Monitor fixing price', 'english', 'prices/Price-Monitors.txt', 1, 'CP1251'),
+(2, 'Мониторы', 'Прайс ремонта мониторов', 'russian', 'prices/Price-Monitors.txt', 1, 'CP1251'),
+(3, 'Notebooks', 'Notebook fixing price', 'english', 'prices/Price-Notebooks.txt', 1, 'CP1251'),
+(4, 'Ноутбуки', 'Прайс ремонта ноутбуков', 'russian', 'prices/Price-Notebooks.txt', 1, 'CP1251'),
+(5, 'Smart phones', 'Smart phones fixing price', 'english', 'prices/Price-Smart-Phones.txt', 1, 'CP1251'),
+(6, 'Смартфоны', 'Прайс ремонта смартфонов', 'russian', 'prices/Price-Smart-Phones.txt', 1, 'CP1251'),
+(7, 'Tablet PC', 'Tablet PC''s fixing price', 'english', 'prices/Price-Tablet.txt', 1, 'CP1251'),
+(8, 'Планшетные ПК', 'Прайс ремонта планшетных ПК', 'russian', 'prices/Price-Tablet.txt', 1, 'CP1251');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -109,7 +185,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `first_name`, `patronymic`, `last_name`, `login`, `password`, `pwd`, `email`, `group`, `active`, `session_id`, `language`, `reg_expired`) VALUES
-(1, 'Morozov', 'Mikhail', 'Sergeevich', 'mcm', 'edf6f38dec4d68e43d05aaba6a6586bc', '784512', 'mcm_@mail.ru', 1, 1, '', '', '2016-12-25 22:10:33');
+(1, 'Morozov', 'Sergeevich', 'Mikhail', 'mcm', 'edf6f38dec4d68e43d05aaba6a6586bc', '784512', 'mcm_@mail.ru', 1, 1, 'i8j2eidsmdagvjgufs0npgps77', '', '2016-12-25 22:10:33');
 
 -- --------------------------------------------------------
 
@@ -136,6 +212,14 @@ INSERT INTO `user_group` (`id`, `name`) VALUES
 --
 
 --
+-- Indexes for table `device`
+--
+ALTER TABLE `device`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_UNIQUE` (`id`),
+  ADD KEY `id_serial` (`serial`);
+
+--
 -- Indexes for table `languages`
 --
 ALTER TABLE `languages`
@@ -154,6 +238,32 @@ ALTER TABLE `news`
 ALTER TABLE `options`
   ADD PRIMARY KEY (`name`),
   ADD UNIQUE KEY `name_UNIQUE` (`name`);
+
+--
+-- Indexes for table `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`idorder`),
+  ADD UNIQUE KEY `idorder_UNIQUE` (`idorder`),
+  ADD KEY `idx_customer` (`customer_id`),
+  ADD KEY `idx_device` (`device_id`);
+
+--
+-- Indexes for table `order_log`
+--
+ALTER TABLE `order_log`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_UNIQUE` (`id`),
+  ADD KEY `idx_order` (`oreder_id`),
+  ADD KEY `idx_customer` (`post_author`,`post_reciver`),
+  ADD KEY `fk_reciver_idx` (`post_reciver`);
+
+--
+-- Indexes for table `prices`
+--
+ALTER TABLE `prices`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_UNIQUE` (`id`);
 
 --
 -- Indexes for table `users`
@@ -177,6 +287,16 @@ ALTER TABLE `user_group`
 --
 
 --
+-- AUTO_INCREMENT for table `order`
+--
+ALTER TABLE `order`
+  MODIFY `idorder` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `prices`
+--
+ALTER TABLE `prices`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
@@ -189,6 +309,21 @@ ALTER TABLE `user_group`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `order`
+--
+ALTER TABLE `order`
+  ADD CONSTRAINT `fk_customer` FOREIGN KEY (`customer_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_device` FOREIGN KEY (`device_id`) REFERENCES `device` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `order_log`
+--
+ALTER TABLE `order_log`
+  ADD CONSTRAINT `fk_author` FOREIGN KEY (`post_author`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_order` FOREIGN KEY (`oreder_id`) REFERENCES `order` (`idorder`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_reciver` FOREIGN KEY (`post_reciver`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `users`
