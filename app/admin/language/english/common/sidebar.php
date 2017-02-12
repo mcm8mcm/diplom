315d065957039e169f036c0ac78d6afbb28ff679
MCM <mcm@mcm-nb>
2017-02-12 23:31:01 +0200

    Admin control panel created v1.2.

--- app/admin/language/english/common/sidebar.php
+++ app/admin/language/english/common/sidebar.php
@@ -1,2 +1,7 @@
 <?php
-$_['news_title'] = 'News & Announcements';
+$_['title'] = 'Options';
+$_['opt_users'] = 'Users control';
+$_['opt_tasks'] = 'Tasks control';
+$_['opt_language'] = 'Language control';
+$_['opt_news'] = 'News control';
+$_['opt_footer'] = 'Footer content control';
