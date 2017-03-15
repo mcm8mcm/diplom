18f938cd040912f61bfa1436cdae6dedadc17f3a
MCM <mcm@mcm-nb>
2017-03-16 00:10:18 +0200

    Control form for "Tasks". Order rendering.

--- app/admin/controller/editors/task_list.php
+++ app/admin/controller/editors/task_list.php
@@ -3,8 +3,7 @@ class ControllerEditorsTaskList extends Controller {
     public function index() {
         $data = array();
         $this->load->model("editors");
-        $data['user_tasks'] = $this->model_editors->getTasks($this->request->get['user_id']);
-        //ddd($data['user_tasks']);        
+        $data['user_tasks'] = $this->model_editors->getTasks($this->request->get['user_id']);      
         return $this->load->view("editors/tasks_list", $data);
     }
 }
---
+++ app/admin/controller/taskrender/log_renderer.php
@@ -0,0 +1,8 @@
+<?php
+class ControllerTaskrendererLogRenderer extends Controller {
+    public function index($data) {
+        //Index is not stand alone in this context
+        //$data is required
+        
+    }
+}
